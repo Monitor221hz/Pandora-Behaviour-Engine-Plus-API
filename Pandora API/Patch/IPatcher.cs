752525e2b3632941c5cc14a6154a665c231b4d41
Monitor <89633536+Monitor221hz@users.noreply.github.com>
2025-11-06 17:30:18 +0000

    refactor: remove unneeded methods

--- Pandora API/Patch/IPatcher.cs
+++ Pandora API/Patch/IPatcher.cs
@@ -15,22 +15,11 @@ public interface IPatcher
     public string GetVersionString();
     public Version GetVersion();
     public void SetTarget(List<IModInfo> mods);
-
     public Task PreloadAsync();
-
-    public void Update();
-
-    public void Run();
-
     public string GetPostRunMessages();
-
     public string GetFailureMessages();
-
     public Task<bool> UpdateAsync();
-
     public Task<bool> RunAsync();
-
     public void SetOutputPath(DirectoryInfo directoryInfo);
-
     public void SetOutputPath(string outputPath) => SetOutputPath(new DirectoryInfo(outputPath));
 }
